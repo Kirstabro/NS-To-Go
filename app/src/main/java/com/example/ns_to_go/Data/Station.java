483d5b8932933cffee23c4c6728e6dcc697ef569
Kirsten <kirstabro@gmail.com>
2019-12-18 14:01:52 +0100

    Stations added + permissions

---
+++ app/src/main/java/com/example/ns_to_go/Data/Station.java
@@ -0,0 +1,108 @@
+package com.example.ns_to_go.Data;
+
+public class Station
+{
+
+    private StationType stationType;
+    private String land;
+    private double lat;
+    private double lng;
+    private String[] names;
+    private String code;
+    private String EVACode;
+    private String UICCODE;
+
+    public Station(StationType stationType, String land, double lat, double lng, String[] names, String code, String EVACode, String UICCODE)
+    {
+        this.stationType = stationType;
+        this.land = land;
+        this.lat = lat;
+        this.lng = lng;
+        this.names = names;
+        this.code = code;
+        this.EVACode = EVACode;
+        this.UICCODE = UICCODE;
+    }
+
+    //region getters and setters
+    public StationType getStationType()
+    {
+        return stationType;
+    }
+
+    public void setStationType(StationType stationType)
+    {
+        this.stationType = stationType;
+    }
+
+    public String getLand()
+    {
+        return land;
+    }
+
+    public void setLand(String land)
+    {
+        this.land = land;
+    }
+
+    public double getLat()
+    {
+        return lat;
+    }
+
+    public void setLat(double lat)
+    {
+        this.lat = lat;
+    }
+
+    public double getLng()
+    {
+        return lng;
+    }
+
+    public void setLng(double lng)
+    {
+        this.lng = lng;
+    }
+
+    public String[] getNames()
+    {
+        return names;
+    }
+
+    public void setNames(String[] names)
+    {
+        this.names = names;
+    }
+
+    public String getCode()
+    {
+        return code;
+    }
+
+    public void setCode(String code)
+    {
+        this.code = code;
+    }
+
+    public String getEVACode()
+    {
+        return EVACode;
+    }
+
+    public void setEVACode(String EVACode)
+    {
+        this.EVACode = EVACode;
+    }
+
+    public String getUICCODE()
+    {
+        return UICCODE;
+    }
+
+    public void setUICCODE(String UICCODE)
+    {
+        this.UICCODE = UICCODE;
+    }
+    //endregion
+}
--- app/src/main/java/com/example/ns_to_go/MainActivity.java
+++ app/src/main/java/com/example/ns_to_go/MainActivity.java
@@ -1,35 +1,69 @@
 package com.example.ns_to_go;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Toast;
+
+import com.example.ns_to_go.Adapters.SectionsPageAdapter;
 import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import com.example.ns_to_go.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SectionsPageAdapter sectionsPageAdapter;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            askPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        this.sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
     }
+
+    public void askPermission(String permission) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{permission},
+                    1);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int i = 0; i < grantResults.length; i++) {
+            if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Location permission not accepted.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+//        adapter.addFrament(new Fragment());
+        viewPager.setAdapter(adapter);
+    }
+
 }
